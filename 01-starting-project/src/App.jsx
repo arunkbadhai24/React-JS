f6ab9f4d1a659f5d031cd32402af53a9db09772d
arunkbadhai24 <arunkb2024@gmail.com>
2024-08-24 12:22:37 +0530

    object destructing

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -23,12 +23,23 @@ function Header() {
   );
 }
 
-function CoreConcept(props){
+// function CoreConcept(props){
+//   return(
+//     <li>
+//         <img src={props.image} alt="..." />
+//         <h3>{props.title}</h3>
+//         <p>{props.description}</p>
+//     </li>
+    
+//   );
+// }
+
+function CoreConcept({image,title,description}){
   return(
     <li>
-        <img src={props.image} alt="..." />
-        <h3>{props.title}</h3>
-        <p>{props.description}</p>
+        <img src={image} alt="..." />
+        <h3>{title}</h3>
+        <p>{description}</p>
     </li>
     
   );
